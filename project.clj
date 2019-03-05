ca011c7c6f0a69fa9ed15f75f24436fe3519a9ea
Trevor Abell <trevor.abell@parsus.com>
2019-03-05 15:52:28 -0700

    change(deps):Switch postgres -> sql server driver

--- project.clj
+++ project.clj
@@ -7,18 +7,18 @@
   :dependencies [[org.clojure/java.classpath "0.3.0"]
                  [org.clojure/java.jdbc "0.7.8"]
                  [org.clojure/tools.logging "0.4.1"]
-                 [org.clojure/tools.namespace "0.2.10"]
+                 [org.clojure/tools.namespace "0.2.11"]
                  [honeysql "0.9.4"]]
   :aliases {"bikeshed" ["bikeshed" "--max-line-length" "118" "--var-redefs" "false"]
             "lint" ["do" ["eastwood"] ["bikeshed"] ["docstring-checker"] ["check-namespace-decls"]]
             "test" ["expectations"]
             "start-db" ["shell" "./start-db"] ; `lein start-db` and stop-db are conveniences for running a test database via Docker
             "stop-db" ["shell" "docker" "stop" "toucan_test"]}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]
                                   [expectations "2.1.10"]
-                                  [postgresql "9.3-1102.jdbc41"]]
+                                  [com.microsoft.sqlserver/mssql-jdbc "6.1.0.jre8"]]
                    :plugins [[docstring-checker "1.0.3"]
-                             [jonase/eastwood "0.3.4"
+                             [jonase/eastwood "0.3.5"
                               :exclusions [org.clojure/clojure]]
                              [lein-bikeshed "0.5.1"]
                              [lein-expectations "0.0.8"]
